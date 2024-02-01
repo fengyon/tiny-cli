fd46bb13475b21d24ea608cca43b5346f643bfff
fengyon <feng.yg@foxmail.com>
2024-01-31 17:42:02 -0800

    fix(toolkits/pro) optimal configuration of mock by vite (#114)

--- packages/toolkits/pro/template/tinyvue/config/vite.config.base.ts
+++ packages/toolkits/pro/template/tinyvue/config/vite.config.base.ts
@@ -1,11 +1,11 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import svgLoader from 'vite-svg-loader';
 import { viteMockServe } from 'vite-plugin-mock';
 
-const useMock = !! process.env.VITE_USE_MOCK
+const useMock = loadEnv('', process.cwd()).VITE_USE_MOCK === 'true'
 
 const config = {
   plugins: [
@@ -14,9 +14,8 @@ const config = {
     svgLoader({ svgoConfig: {} }),
     viteMockServe({
       mockPath: '../src/mock', // mock文件地址
-      localEnabled:useMock, // 开发打包开关
+      localEnabled: useMock, // 开发打包开关
       prodEnabled:true, // 生产打包开关 // 这样可以控制关闭mock的时候不让mock打包到最终代码内
-      injectCode: ` import { setupProdMockServer } from '../src/mockProdServer'; setupProdMockServer(); `,
       logger: true, // 是否在控制台显示请求日志
       supportTs: false, // 打开后，可以读取 ts 文件模块。 请注意，打开后将无法监视.js 文件
       watchFiles:true,
--- packages/toolkits/pro/template/tinyvue/src/main.ts
+++ packages/toolkits/pro/template/tinyvue/src/main.ts
@@ -6,7 +6,6 @@ import router from './router';
 import store from './store';
 import i18n from './locale';
 import directive from './directive';
-import { setupProdMockServer } from './mockProdServer';
 import './mock';
 import App from './App.vue';
 import '@/api/interceptor';
@@ -16,7 +15,6 @@ import config from '../hwc-exports.json';
 import 'echarts4/map/js/china.js';
 import chinaMap from './assets/chaina.json';
 
-if(import.meta.env.VITE_USE_MOCK) setupProdMockServer();
 echarts4.registerMap('china', chinaMap);
 const app = createApp(App);
 
